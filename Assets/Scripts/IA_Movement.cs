f719f4d78d9921d8e339d7b44ad39b834fda8b30
Ash <afalzon12@gmail.com>
2023-06-12 16:38:40 +0200

    Implementation for IA vs IA + offsets corrections

--- Assets/Scripts/IA_Movement.cs
+++ Assets/Scripts/IA_Movement.cs
@@ -22,6 +22,9 @@ public class IA_Movement : MonoBehaviour
     private float speed;
     private float smoothingSpeed = 10f;
 
+    private float previousPosition;
+    private bool towardsMe;
+
     [SerializeField] private bool debug = false;
 
     // Start is called before the first frame update
@@ -29,17 +32,28 @@ public class IA_Movement : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody2D>();
+
+        previousPosition = 0;
+
+        // Store the initial position as the previous position
         InitializeAIDifficulty();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // move the paddle
+        AIGestion(towardsMe);
+    }
+
+    private void FixedUpdate()
+    {
         // check for the ball
         ball = GameObject.FindGameObjectWithTag("Ball");
         ballRb = ball.GetComponent<Rigidbody2D>();
 
-        AIGestion();
+        // check if the ball is coming
+        IsTheBallMovingTowardsMe();
     }
 
     private void InitializeAIDifficulty()
@@ -62,12 +76,35 @@ public class IA_Movement : MonoBehaviour
         }
     }
 
-    private void AIGestion()
+    private void IsTheBallMovingTowardsMe()
+    {
+        // Get the current position
+        float currentPosition = ball.transform.position.x;
+
+        // Calculate the displacement between the current position and the previous position
+        float displacement = currentPosition - previousPosition;
+
+        if (debug) Debug.Log(displacement);
+
+        // Update the previous position for the next frame
+        previousPosition = currentPosition;
+
+        if ((displacement < 0 && this.transform.position.x < 0) || (displacement > 0 && this.transform.position.x > 0))
+        {
+            towardsMe = true;
+        }
+        else
+        {
+            towardsMe= false;
+        }
+    }
+
+    private void AIGestion(bool towardsMe)
     {
         // this checks if the ball is moving
         if (ballRb.velocity.magnitude != 0)
         {
-            if (ballRb.velocity.x > 0) // if not, move only when the ball comes towards you
+            if (towardsMe) // move only when the ball comes towards you
             {
                 if (difficulty == 0) // use the simple follow the ball algorythm for easy mode
                 {
@@ -101,7 +138,6 @@ public class IA_Movement : MonoBehaviour
         if (yPosition > this.transform.position.y)
         {
             return Vector2.up;
-
         }
         // same but if the ball is lower
         else if (yPosition < this.transform.position.y)
@@ -112,6 +148,25 @@ public class IA_Movement : MonoBehaviour
         else
         {
             return Vector2.zero;
+        }
+    }
+
+    private bool IsTheBallGoingUpwards(float yPosition)
+    {
+        // if the ball is higher than the paddle make the paddle move smoothly towards the balls height
+        if (yPosition > this.transform.position.y)
+        {
+            return true;
+        }
+        // same but if the ball is lower
+        else if (yPosition < this.transform.position.y)
+        {
+            return false;
+        }
+        // else just stop
+        else
+        {
+            return false;
         }
     }
 
@@ -150,14 +205,32 @@ public class IA_Movement : MonoBehaviour
             // Perform the reflection raycast off of the wall
             RaycastHit2D reflectionHit = Physics2D.Raycast(hit.point, reflectionDirection, maxDistance, layerMaskHit);
 
-            if(reflectionHit) impact = reflectionHit.point.y; // if it hits something tell the paddle so it knows
+            if (reflectionHit)
+            {
+                impact = reflectionHit.point.y + GetOffset(reflectionHit, direction, hit.normal); // if it hits something tell the paddle so it knows
+            } 
         }
         else
         {
             hit = Physics2D.Raycast(origin, direction, maxDistance, layerMaskHit);
-            impact = hit.point.y;
+            
+            impact = hit.point.y + GetOffset(hit, direction, hit.normal);
         }
-
         return impact;
+    }
+
+    private float GetOffset(RaycastHit2D hit, Vector3 direction, Vector3 normal)
+    {
+        float angle = Vector3.Angle(normal, direction);
+        float offset = 0.5f * Mathf.Tan(angle);
+
+        if (IsTheBallGoingUpwards(ball.transform.position.y))
+        {
+            return -offset;
+        }
+        else
+        {
+            return offset;
+        }
     }
 }
